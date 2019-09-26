ccb8737af90f974794af84de4262b62df82335f6
Kenold Beauplan <bkenold@gmail.com>
2019-09-26 15:50:30 -0400

    install and configure github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `CSS UI`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    pathPrefix: "/css-ui"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
