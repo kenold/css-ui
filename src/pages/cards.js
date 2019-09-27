715b493bd5cd9b923819c28d692d61cff4a94374
Kenold Beauplan <bkenold@gmail.com>
2019-09-27 11:14:58 -0400

    import, display components

--- src/pages/cards.js
+++ src/pages/cards.js
@@ -1,18 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 import Blog from '../components/cards/blog';
+import BlogWide from '../components/cards/blogWide';
+import Movie from '../components/cards/movie';
+import Product from '../components/cards/product';
+import Video from '../components/cards/video';
 import '../styles/cards.scss';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Cards = () => (
-  <Layout>
-    <SEO title="Cards" />
-    <h1>Cards</h1>
-    <Blog />
-
-  </Layout>
+	<Layout>
+		<SEO title="Cards" />
+		<h1>Cards</h1>
+		<Blog />
+		<Movie />
+		<Product />
+		<Video />
+		<BlogWide />
+	</Layout>
 )
 
 export default Cards
