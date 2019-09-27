2e8cf214499738e25a1c24822423f94f99653074
Kenold Beauplan <bkenold@gmail.com>
2019-09-27 16:33:40 -0400

    add link to image

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>CSS UI Components</h1>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container">
+      <h1>CSS UI Components</h1>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Link to="cards"><Image /></Link>
+      </div>
+      <Link to="cards">Cards</Link>
     </div>
-    <Link to="cards">Cards</Link>
   </Layout>
 )
 
