a66f9ef033708ceac0092cbae6f6c94df6e45c19
Kenold Beauplan <bkenold@gmail.com>
2019-09-27 10:38:29 -0400

    initial commit

---
+++ src/components/cards/blog.js
@@ -0,0 +1,39 @@
+import React from "react"
+import PostData from '../../data/posts.json';
+
+const Blog = () => (
+
+    <section className="l-cards container">
+    <div className="l-cards__heading">
+        <h4>Cards + Blog Posts </h4>
+    </div>
+
+    <div className="cards cards--blog">
+        {PostData.map((post) => (
+            <article key={post.id} className="card grid-4">
+                <a href="#">
+                    <figure className="card__media">
+                        <img src={"/blog/" + post.id + ".jpg"} alt={post.title} className="card__image" />
+                    </figure>
+                    <div className="card__body">
+                        <div className="card__category">{post.category}</div>
+                        <div className="card__title" title={post.title}>
+                            <span>{post.title}</span>
+                        </div>
+                    </div>
+                    <div className="card__footer">
+                        <div className="card__author">{post.author}</div>
+                        <div className="card__date">5 hours ago</div>
+                    </div>
+                </a>
+            </article>
+        ))}
+    </div>
+
+    <div className="code">
+        {/* <BlogCode /> */}
+    </div>
+    </section>
+)
+
+export default Blog
