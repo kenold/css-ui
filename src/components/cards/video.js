df228d7697d40bdd2ac050d08e07e5e8fbddeb31
Kenold Beauplan <bkenold@gmail.com>
2019-09-27 13:39:22 -0400

    update slider settings

--- src/components/cards/video.js
+++ src/components/cards/video.js
@@ -9,32 +9,44 @@ import "slick-carousel/slick/slick-theme.css";
 class Video extends Component {
     render() {
         // slick carousel
-		const settings = {
-			arrows: true,
-			infinite: false,
-			slidesToShow: 3.5,
-			slidesToScroll: 1,
-			initialSlide: 0,
-			autoplay: true,
-      		speed: 2000,
-			responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 3.5,
-					slidesToScroll: 1,
-					arrows: true,
-				}
-				},
-				{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2.5,
-					slidesToScroll: 1
-				}
-			}
-			]
-		};
+		var settings = {
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 3.5,
+            slidesToScroll: 2,
+            initialSlide: 0,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2.5,
+                  slidesToScroll: 2,
+                  initialSlide: 2,
+                  arrows: false,
+                  dots: false
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1.5,
+                  slidesToScroll: 1,
+                  arrows: false,
+                  dots: false
+                }
+              }
+            ]
+        };
         return (
             <section className="l-cards container">
                 <div className="l-cards__heading">
