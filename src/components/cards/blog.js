3de64771e4f25c689e8354faba85d9685c7863c7
Kenold Beauplan <something4ken@gmail.com>
2019-09-29 16:24:45 -0400

    add heading

--- src/components/cards/blog.js
+++ src/components/cards/blog.js
@@ -3,37 +3,36 @@ import BlogCode from '../codes/blog';
 import BlogData from '../../data/posts.json';
 
 const Blog = () => (
-
     <section className="l-cards container">
-    <div className="l-cards__heading">
-        <h4>Cards + Blog Posts </h4>
-    </div>
+        <div className="l-cards__heading">
+            <h4>Cards + Blog Posts </h4>
+        </div>
 
-    <div className="cards cards--blog">
-        {BlogData.map((post) => (
-            <article key={post.id} className="card grid-4">
-                <a href="#">
-                    <figure className="card__media">
-                        <img src={"/blog/" + post.id + ".jpg"} alt={post.title} className="card__image" />
-                    </figure>
-                    <div className="card__body">
-                        <div className="card__category">{post.category}</div>
-                        <div className="card__title" title={post.title}>
-                            <span>{post.title}</span>
+        <div className="cards cards--blog">
+            {BlogData.map((post) => (
+                <article key={post.id} className="card show-2-4">
+                    <a href="#">
+                        <figure className="card__media">
+                            <img src={"/blog/" + post.id + ".jpg"} alt={post.title} className="card__image" />
+                        </figure>
+                        <div className="card__body">
+                            <div className="card__category">{post.category}</div>
+                            <div className="card__title" title={post.title}>
+                                <span>{post.title}</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="card__footer">
-                        <div className="card__author">{post.author}</div>
-                        <div className="card__date">5 hours ago</div>
-                    </div>
-                </a>
-            </article>
-        ))}
-    </div>
+                        <div className="card__footer">
+                            <div className="card__author">{post.author}</div>
+                            <div className="card__date">5 hours ago</div>
+                        </div>
+                    </a>
+                </article>
+            ))}
+        </div>
 
-    <div className="code">
-        <BlogCode />
-    </div>
+        <div className="code">
+            <BlogCode />
+        </div>
     </section>
 )
 
