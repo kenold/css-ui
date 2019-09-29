042711aa88a0c703a2db9b59f4c63d74e9cb54d7
Kenold Beauplan <something4ken@gmail.com>
2019-09-29 16:22:40 -0400

    initial commit

--- src/components/cards/videoSlider.js
+++ src/components/cards/videoSlider.js
@@ -2,17 +2,59 @@ import React, { Component } from "react";
 import { FaPlay } from 'react-icons/fa';
 import VideoData from '../../data/videos.json';
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 class Video extends Component {
     render() {
+        // slick carousel
+		var settings = {
+            dots: false,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 3.5,
+            slidesToScroll: 2,
+            initialSlide: 0,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2.5,
+                  slidesToScroll: 2,
+                  initialSlide: 2,
+                  arrows: false,
+                  dots: false
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1.5,
+                  slidesToScroll: 1,
+                  arrows: false,
+                  dots: false
+                }
+              }
+            ]
+        };
         return (
             <section className="l-cards container">
                 <div className="l-cards__heading">
                     <h4>Cards + Video</h4>
                 </div>
-
-				<div className="cards">
+                <Slider {...settings}>
                 {VideoData.map((video) => (
-                    <article key={video.id} className="card card--video show-1-3">
+                    <article key={video.id} className="card card--video">
                         <a href="#">
                             <figure className="card__media">
                                 <img src={"/videos/" + video.id + ".jpg"} alt={video.title} className="card__image" />
@@ -28,7 +70,7 @@ class Video extends Component {
                         </a>
                     </article>
                 ))}
-				</div>
+                </Slider>
             </section>
         )
     }
