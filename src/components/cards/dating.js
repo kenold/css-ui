cc65d3b3bdcab212b7a64484c6228ee7f1babb36
Kenold Beauplan <bkenold@gmail.com>
2019-10-01 11:00:11 -0400

    update classes, add svg icons

--- src/components/cards/dating.js
+++ src/components/cards/dating.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaHeart, FaThumbsDown } from 'react-icons/fa';
 import DatingData from '../../data/models.json';
 
 const Dating = () => (
@@ -21,9 +20,9 @@ const Dating = () => (
                                 {model.name}, {model.age}
                             </div>
                             <div className="card__text">{model.profession}</div>
-                            <ul className="card__actions cta">
-                                <li className="cta__dislike"><a href="#"><span className="icon"><FaThumbsDown /></span></a></li>
-                                <li className="cta__like"><a href="#"><span className="icon"><FaHeart /></span></a></li>
+                            <ul className="card__actions swipe">
+                                <li className="swipe__left"><a href="#dislike" title="Dislike"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></a></li>
+                                <li className="swipe__right"><a href="#like" title="Like"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg></a></li>
                             </ul>
                         </div>
                     </a>
