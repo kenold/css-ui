7d4789f01f20235817f622854237e39796d7d54a
Kenold Beauplan <something4ken@gmail.com>
2019-10-04 19:41:12 -0400

    update description, author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `CSS UI`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `CSS UI components created by Kenold Beauplan.`,
+    author: `@kenoldb`,
     pathPrefix: "/css-ui"
   },
   plugins: [
