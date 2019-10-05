6cb564f37f0f7de8da3d61a9e850b69bce1d8613
Kenold Beauplan <something4ken@gmail.com>
2019-10-05 12:44:59 -0400

    add new class

--- src/components/Card/OverlayDating.js
+++ src/components/Card/OverlayDating.js
@@ -8,16 +8,19 @@ const OverlayDating = () => (
         </div>
         <div className="cards">
             {DatingData.map((model) => (
-                <article key={model.id} className="card card--overlay show-1-3">
+                <article key={model.id} className="card card--overlay card--overlay--dating show-1-3">
                     <div className="card__image">
                         <a href="#movie-detail" title={model.title}>
                             <img src={"/models/" + model.id + ".jpg"} alt={model.title} />
                         </a>
                     </div>
                     <div className="darken"></div>
-                    <div className="card__content card__content--dating">
-                        <div className="card__title" title={model.name}>
-                            <a href="#profile" className="model">{model.name}, {model.age}</a>
+                    <div className="card__content">
+                        <div className="card__title model" title={model.name}>
+                            <a href="#profile" className="model__link">
+                                <span className="model__name">{model.name}, </span>
+                                <span className="model__age">{model.age}</span>
+                            </a>
                         </div>
                         <div className="card__text">{model.profession}</div>
                         <div className="card__actions swipe">
