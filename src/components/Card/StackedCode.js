88f829f6aa3b923959839aa54f8c345875fc3f04
Kenold Beauplan <something4ken@gmail.com>
2019-10-04 23:12:08 -0400

    rename file

---
+++ src/components/Card/StackedCode.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+//import Prism from 'prismjs';
+import Prism from 'prismjs';
+import '../../../static/prism.css';
+
+const code= `
+    <div class="card">
+        <figure class="card__media">
+            <a href="...">
+                <img src="..." alt="" class="card__image" />
+            </a>
+        </figure>
+        <div class="card__body">
+            <div class="card__topic">Blog</div>
+            <div class="card__title">
+                <a href="...">Card Title</a>
+            </div>
+        </div>
+        <div class="card__footer">
+            <div class="card__author">Jane Doe</div>
+            <div class="card__date">5 hours ago</div>
+        </div>
+    </div>
+`
+class StackedCode extends Component {
+    componentDidMount() {
+        Prism.highlightAll();
+    }
+    render() {
+        return (
+            <>
+            <pre>
+                <code className="language-html">
+                    {code}
+                </code>
+            </pre>
+            </>
+        );
+    }
+}
+
+export default StackedCode
