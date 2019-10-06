e34f4b7c8376689f61c40ab70a9a215d837a4571
Kenold Beauplan <something4ken@gmail.com>
2019-10-06 19:52:04 -0400

    replace svg with font awesome

--- src/components/Card/OverlayDating.js
+++ src/components/Card/OverlayDating.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaTimes, FaHeart } from 'react-icons/fa';
 import DatingData from '../../data/models.json';
 
 const OverlayDating = () => (
@@ -25,10 +26,10 @@ const OverlayDating = () => (
                         <div className="card__text">{model.profession}</div>
                         <div className="card__actions swipe">
                             <a href="#dislike" title="Dislike">
-                                <div className="swipe__left"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></div>
+                                <div className="swipe__left"><FaTimes /></div>
                             </a>
                             <a href="#like" title="Like">
-                                <div className="swipe__right"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg></div>
+                                <div className="swipe__right"><FaHeart /></div>
                             </a>
                         </div>
                     </div>
