08f272057dd8326948be2cfd0f210fa62a0545b4
Kenold Beauplan <bkenold@gmail.com>
2019-10-07 11:03:14 -0400

    initial commit

---
+++ src/components/Card/StackedFloat.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { FaPlay } from 'react-icons/fa';
+import MovieData from '../../data/movies.json';
+
+const StackedFloat = () => (
+    <section className="l-cards container">
+        <div className="l-cards__heading">
+            <h4>Stacked &rarr; Floating Icon</h4>
+        </div>
+
+        <div className="cards">
+            {MovieData.slice(0, 3).map((movie) => (
+                <article key={movie.id} className="card card--float show-1-3">
+                    <div className="card__image">
+                        <img src={"/movies/" + (movie.id) + ".jpg"} alt={movie.title} />
+                        <a href="#action" className="card__icon-link btn__circle btn__circle--float-rb">
+                            <div className="card__icon"><FaPlay /></div>
+                        </a>
+                    </div>
+                    <div className="card__content">
+                        <header className="card__header">
+                            <h4 className="card__title" title={movie.title}>
+                                <a href="#blog-detail">{movie.title}</a>
+                            </h4>
+                            <p className="card__text">{movie.synopsis}</p>
+                        </header>
+                        <div className="card__text">{movie.synosis}</div>
+                    </div>
+                </article>
+            ))}
+        </div>
+    </section>
+)
+
+export default StackedFloat
