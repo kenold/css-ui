a70c31243f7dfe60cacfb330cfcb0673c351c630
Kenold Beauplan <something4ken@gmail.com>
2019-10-06 20:21:14 -0400

    nest footer inside content

--- src/components/Card/Testimonial.js
+++ src/components/Card/Testimonial.js
@@ -14,15 +14,15 @@ const Testimonial = () => (
                 <article key={testimonial.id} className="card card--testimonial show-1-3">
                     <div className="card__content">
                         <p className="card__text">"{testimonial.text}"</p>
+                        <footer className="card__footer author">
+                            <div className="author__photo">
+                                <img src={"/testimonials/" + testimonial.id + ".jpg"} alt={testimonial.authorName} className="author__image" />
+                            </div>
+                            <div className="author__name">
+                                {testimonial.authorName} <br /> {testimonial.authorPosition}
+                            </div>
+                        </footer>
                     </div>
-                    <footer className="card__footer author">
-                        <div className="author__photo">
-                            <img src={"/testimonials/" + testimonial.id + ".jpg"} alt={testimonial.authorName} className="author__image" />
-                        </div>
-                        <div className="author__name">
-                            {testimonial.authorName} <br /> {testimonial.authorPosition}
-                        </div>
-                    </footer>
                 </article>
             ))}
         </div>
