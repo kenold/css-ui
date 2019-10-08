79317d59d61c83a6979c788ba2f609d20ddcc476
Kenold Beauplan <bkenold@gmail.com>
2019-10-08 17:09:42 -0400

    import card to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
+import Card from './card';
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="container">
-      <h1>CSS UI Components</h1>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Link to="/card"><Image /></Link>
-      </div>
-      <Link to="/card">Cards</Link>
-    </div>
-  </Layout>
+  // <Layout>
+  //   <SEO title="Home" />
+  //   <div className="container">
+  //     <h1>CSS UI Components</h1>
+  //     <p>Now go build something great.</p>
+  //     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //     <Link to="/card"><Image /></Link>
+  //     </div>
+  //     <Link to="/card">Cards</Link>
+  //   </div>
+  // </Layout>
+
+  <Card />
+
 )
 
 export default IndexPage
