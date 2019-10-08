92c4e5b8d09f5928c45c3e65ab08987e41336a4c
Kenold Beauplan <bkenold@gmail.com>
2019-10-08 19:32:46 -0400

    import new component

--- src/pages/card.js
+++ src/pages/card.js
@@ -2,6 +2,7 @@ import React from "react"
 
 // cards component
 import Stacked from '../components/Card/Stacked';
+import StackedOverlap from '../components/Card/StackedOverlap';
 import StackedTour from '../components/Card/StackedTour';
 import StackedExpand from '../components/Card/StackedExpand';
 import StackedMinimal from '../components/Card/StackedMinimal';
@@ -26,6 +27,7 @@ const Card = () => (
 	<Layout>
 		<SEO title="Cards" />
 		<Stacked />
+		<StackedOverlap />
 		<StackedTour />
 		<StackedExpand />
 		<StackedLabel />
