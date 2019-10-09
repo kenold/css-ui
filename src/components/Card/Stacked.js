a2b2f3110d1788064e004ac9cc5a07f4681a0a82
Kenold Beauplan <bkenold@gmail.com>
2019-10-09 10:55:04 -0400

    use card components

--- src/components/Card/Stacked.js
+++ src/components/Card/Stacked.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-import Moment from 'react-moment';
 import StackedCode from './StackedCode';
 import BlogData from '../../data/posts.json';
+
+import CardImage from './Image'
+import CardContent from './Content'
+import CardHeading from './Heading'
+//import Card from './Card'
 
 class Stacked extends Component {
     constructor (props) {
@@ -19,50 +23,22 @@ class Stacked extends Component {
         return (
             <>
             <section className="l-cards container" id="stacked">
-                <div className="l-cards__heading">
-                    <h4>Card: Stacked</h4>
-
-                    <div className="show-code" title="Display Code" onClick={this.toggleHidden.bind(this)}>
-
-                        {this.state.isHidden ? (
-                            <a href="#code">
-                                <div className="code-button">
-                                    &lt; Show Code &gt;
-                                </div>
-                            </a>
-                        ) : (
-                            <a href="#stacked">
-                                <div className="code-button">
-                                    &lt; Hide Code &gt;
-                                </div>
-                            </a>
-                        )}
-                    </div>
-                </div>
+                <CardHeading heading="Stacked" />
 
                 <div className="cards">
                     {BlogData.slice(0,3).map((post) => (
-                        <article key={post.id} className="card show-1-3">
-                            <div className="card__image">
-                                <a href="#blog-detail">
-                                    <img src={"/blog-wide/" + (post.id+8) + ".jpg"} alt={post.title} />
-                                </a>
-                            </div>
-                            <div className="card__content">
-                                <div className="card__topic">{post.topic}</div>
-                                <header className="card__header">
-                                    <h4 className="card__title" title={post.title}>
-                                        <a href="#blog-detail">{post.title}</a>
-                                    </h4>
-                                </header>
-                                <footer className="card__footer">
-                                    <div className="card__author">by {post.author}</div>
-                                    <div className="card__date">
-                                        <Moment fromNow>{post.date}</Moment>
-                                    </div>
-                                </footer>
-                            </div>
-                        </article>
+                    <article className="card show-1-3">
+                        <CardImage
+                            title={post.title}
+                            image={"/blog-wide/" + (post.id+8) + ".jpg"}
+                        />
+                        <CardContent
+                            topic={post.topic}
+                            title={post.title}
+                            author={post.author}
+                            date={post.date}
+                        />
+                    </article>
                     ))}
                 </div>
             </section>
