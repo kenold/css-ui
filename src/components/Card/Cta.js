32f81d21756a5ba47ce218bfb2677f55853c86a6
Kenold Beauplan <something4ken@gmail.com>
2019-10-10 21:46:41 -0400

    move loop to CardList

--- src/components/Card/Cta.js
+++ src/components/Card/Cta.js
@@ -1,29 +1,27 @@
 import React from "react"
-import CtaData from '../../data/ctas.json';
-import Heading from "./Heading.js";
 
-const Cta = () => (
-    <section className="l-cards l-cards--cta container">
-        <Heading title="Call-to-Action" />
-
-        <div className="cards">
-            {CtaData.slice(0, 3).map((cta) => (
-                <article key={cta.id} className="card card--cta show-1-3">
-                    <div className="card__content">
-                        <header className="card__header">
-                            <h4 className="card__title" title={cta.title}>
-                                <a href="#blog-detail">{cta.title}</a>
-                            </h4>
-                        </header>
-                        <div className="card__text">{cta.text}</div>
-                        <footer className="card__footer btn">
-                        { cta.btn1 && <a href="#signup"><div className="btn--primary">{cta.btn1}</div></a> }
-                        { cta.btn2 && <a href="#learn-more"><div className="btn--secondary">{cta.btn2}</div></a> }
-                        </footer>
-                    </div>
-                </article>
-            ))}
+const Cta = (props) => (
+    <article key={props.id} className={"card card--cta " + props.grid}>
+        <div className="card__content">
+            <header className="card__header">
+                <h4 className="card__title" title={props.title}>
+                    <a href="#blog-detail">{props.title}</a>
+                </h4>
+            </header>
+            <div className="card__text">{props.text}</div>
+            <footer className="card__footer">
+                { props.btn1 &&
+                    <a href="#signup">
+                        <div className="btn btn--primary">{props.btn1}</div>
+                    </a>
+                }
+                { props.btn2 &&
+                    <a href="#learn-more">
+                        <div className="btn btn--secondary">{props.btn2}</div>
+                    </a>
+                }
+            </footer>
         </div>
-    </section>
+    </article>
 )
 export default Cta
