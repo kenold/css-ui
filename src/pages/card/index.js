a57fbf9bbab7be9d2023835d612817dc6f79fa84
Kenold Beauplan <something4ken@gmail.com>
2019-10-13 12:26:16 -0400

    initial commit

---
+++ src/pages/card/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from "~components/layout"
+import SEO from "~components/seo"
+
+const CardCodePage = () => {
+    return (
+        <Layout>
+		    <SEO title="Cards" />
+            <div className="container">
+                card code instructions here
+            </div>
+        </Layout>
+    )
+}
+
+export default CardCodePage
