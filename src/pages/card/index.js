c758c103feb517d775c47b8460b3e46984d56444
Kenold Beauplan <something4ken@gmail.com>
2019-10-13 13:29:00 -0400

    import new component

--- src/pages/card/index.js
+++ src/pages/card/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import StackedBasic from '~components/Card/Stacked/Basic'
+import StackedBasicCode from '~components/Code/Stacked/Basic'
 import Layout from "~components/layout"
 import SEO from "~components/seo"
+import Section from '~components/section'
 
 const CardIndexPage = () => {
     return (
         <Layout>
-		    <SEO title="Cards" />
-            <div className="container">
-                card code instructions here
-            </div>
+		    <SEO title="Card UI Instructions" />
+            <Section title="Cards UI" cssClasses="l-ui l-ui--stacked">
+                <div className="ui">
+                    <div className="ui__visual">
+                        <StackedBasic
+                            id={8}
+                            title="Card Title"
+                            topic="Topic"
+                            author="Jane Doe"
+                            date="4/21/2019"
+                        />
+                    </div>
+                    <div className="ui__code">
+                        <StackedBasicCode />
+                    </div>
+                </div>
+            </Section>
         </Layout>
     )
 }
