5ae266baa65ecc66baee90368de92fe688744c85
Kenold Beauplan <bkenold@gmail.com>
2019-10-14 11:07:28 -0400

    initial commit

---
+++ src/components/Code/Stacked/Overlap.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {PrismCode} from "~components/prismCode"
+import Section from '~components/section'
+import StackedOverlap from '~components/Card/Stacked/Overlap'
+
+const code= `
+<div class="card card--overlap">
+    <div class="card__image"><img src="..." /></div>
+    <div class="card__content">
+        <header class="card__header">
+            <h4 class="card__title">Card Title</h4>
+        </header>
+        <div class="card__footer">
+            <div class="card__date">6 months ago</div>
+        </div>
+    </div>
+</div>
+`.trim()
+export const StackedOverlapCode = () => (
+    <Section title="Stacked Overlap" cssClasses="l-ui l-ui--stacked-overlap">
+        <div className="ui">
+            <div className="ui__visual">
+                <StackedOverlap
+                    id="1"
+                    title="Card Title"
+                    date="4/21/2019"
+                />
+            </div>
+            <div className="ui__code">
+                <PrismCode
+                    code={code}
+                    language="html"
+                />
+            </div>
+        </div>
+    </Section>
+)
---
+++ src/components/Code/Stacked/Tour.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {PrismCode} from "~components/prismCode"
+import Section from '~components/section'
+import StackedTour from '~components/Card/Stacked/Tour'
+
+const code= `
+<div class="card card--tour">
+	<div class="card__image"><img src="..." /></div>
+	<div class="card__content">
+		<header class="card__header">
+			<div class="card__topic">
+				<i class="fas fa-map-marker-alt"></i> /> Paris
+			</div>
+			<h4 class="card__title" >Eiffel Tower</h4>
+		</header>
+        <p class="card__text card__text--small">
+            Lorem sapien arcu sed augue...
+        </p>
+		<div class="card__subtext"><span>from</span> $299</div>
+	</div>
+</div>
+`.trim()
+export const StackedTourCode = () => (
+    <Section title="Stacked Tour" cssClasses="l-ui l-ui--stacked-tour">
+        <div className="ui">
+            <div className="ui__visual">
+                <StackedTour
+                    cssClasses="card--tour"
+                    id="3"
+                    title="Eiffel Tower"
+                    topic="Paris"
+                    text="Lorem sapien arcu sed augue..."
+                    price="$299"
+                />
+            </div>
+            <div className="ui__code">
+                <PrismCode
+                    code={code}
+                    language="html"
+                />
+            </div>
+        </div>
+    </Section>
+)
