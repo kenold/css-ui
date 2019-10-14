0cf7c2f6371f36d12f4ed6592ba85edcacbabcbd
Kenold Beauplan <bkenold@gmail.com>
2019-10-14 10:42:17 -0400

    import social code

--- src/pages/card/index.js
+++ src/pages/card/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { StackedBasicCode } from '~components/Code/Stacked/Basic'
 import Layout from "~components/layout"
 import SEO from "~components/seo"
+import { StackedBasicCode } from '~components/Code/Stacked/Basic'
+import { StackedSocialCode } from '~components/Code/Stacked/Social'
 
 const CardIndexPage = () => {
     return (
         <Layout>
 		    <SEO title="Card UI Instructions" />
             <StackedBasicCode />
+            <StackedSocialCode />
         </Layout>
     )
 }
