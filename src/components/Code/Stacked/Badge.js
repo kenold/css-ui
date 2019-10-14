c9cc9b83bfe0508d40fe38252d1a11aeb7967100
Kenold Beauplan <bkenold@gmail.com>
2019-10-14 12:25:15 -0400

    initial commit

---
+++ src/components/Code/Stacked/Badge.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {PrismCode} from "~components/prismCode"
+import Section from '~components/section'
+import StackedBadge from '~components/Card/Stacked/Badge'
+
+const code= `
+<div class="card card--badge">
+    <div class="card__image"><img src="..." /></div>
+    <div class="card__content">
+        <header class="card__header">
+            <div class="card__topic">Fashion</div>
+            <h4 class="card__title">Card Title</h4>
+        </header>
+        <div className="card__text">Lorem...</div>
+        <div class="card__footer">
+            <div class="card__author">by Jane Doe</div>
+            <div class="card__date">6 months ago</div>
+        </div>
+    </div>
+</div>
+`.trim()
+export const StackedBadgeCode = () => (
+    <Section title="Stacked Badge" cssClasses="l-ui l-ui--stacked-badge">
+        <div className="ui">
+            <div className="ui__visual">
+                <StackedBadge
+                    id="5"
+                    title="Card Title"
+                    topic="Fashion"
+                    date="4/21/2019"
+                />
+            </div>
+            <div className="ui__code">
+                <PrismCode
+                    code={code}
+                    language="html"
+                />
+            </div>
+        </div>
+    </Section>
+)
---
+++ src/components/Code/Stacked/FloatingIcon.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {PrismCode} from "~components/prismCode"
+import Section from '~components/section'
+import StackedFloatingIcon from '~components/Card/Stacked/FloatingIcon'
+
+const code= `
+<div class="card card--float">
+    <div class="card__image">
+        <img src="..." />
+        <a href="#" class="card__icon-link btn btn--circle">
+            <div class="card__icon">
+                <i class="fas fa-play"></i>
+            </div>
+        </a>
+    </div>
+    <div class="card__content">
+        <header class="card__header">
+            <div class="card__topic">Thriller</div>
+            <h4 class="card__title">Card Title</h4>
+        </header>
+        <div className="card__text">Lorem...</div>
+    </div>
+</div>
+`.trim()
+export const StackedFloatingIconCode = () => (
+    <Section title="Stacked Floating Icon" cssClasses="l-ui l-ui--stacked-float">
+        <div className="ui">
+            <div className="ui__visual">
+                <StackedFloatingIcon
+                    id="6"
+                    title="Card Title"
+                    topic="Thriller"
+                    text="Lorem..."
+                />
+            </div>
+            <div className="ui__code">
+                <PrismCode
+                    code={code}
+                    language="html"
+                />
+            </div>
+        </div>
+    </Section>
+)
