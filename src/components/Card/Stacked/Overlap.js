e28997a05725b728129f9276a5d00ad016d5117a
Kenold Beauplan <something4ken@gmail.com>
2019-10-14 21:20:22 -0400

    add overlap full

--- src/components/Card/Stacked/Overlap.js
+++ src/components/Card/Stacked/Overlap.js
@@ -41,3 +41,39 @@ export const StackedOverlapList = (props) => (
         </div>
     </Section>
 )
+
+// OVERLAP FULL
+export const StackedOverlapFull = (props) => (
+    <article className={"card card--overlap-full" + (props.cssClasses ? " " + props.cssClasses:"")}>
+        <div className="card__image">
+            <a href="#blog-detail">
+                <img src={"/ctas/" + props.id + ".jpg"} alt={props.title} />
+            </a>
+        </div>
+        <div className="card__content">
+            <header className="card__header">
+                <h4 className="card__title" title={props.title}>
+                    <a href="#blog-detail">{props.title}</a>
+                </h4>
+            </header>
+            <footer className="card__footer">
+                <div className="card__date">
+                    <Moment fromNow>{props.date}</Moment>
+                </div>
+            </footer>
+        </div>
+    </article>
+)
+
+export const StackedOverlapFullList = (props) => (
+    <Section title="Overlap" cssClasses="l-cards--stacked-overlap-full">
+        {BlogData.slice(0,1).map((post) => (
+            <StackedOverlapFull
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                date={post.date}
+            />
+        ))}
+    </Section>
+)
